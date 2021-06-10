c9de61278a722c0f988fda2a4b5fe836a190c5f4
jasjot Singh <jasjotb589@gmail.com>
2021-06-10 16:10:34 +0530

    added hashing of the password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express=require('express');
 const router=express.Router();
 const userModel=require('../models/authModel')
 const Joi=require('@hapi/joi')
+const bcrypt=require('bcryptjs')
 const {validation}=require('./validation')
 
 //Post route for the process
@@ -17,12 +18,16 @@ router.post('/resgistration',async(req,res)=>{
       res.send('sorry the email is already associated with another account')
       return
   }
+const salt=await bcrypt.genSalt(10);
+
+const hashedPass=await bcrypt.hash(req.body.password,salt);
+
 
 
     
     let user=new userModel({
         user:req.body.user,
-        password:req.body.password,
+        password:hashedPass,
         email:req.body.email
     })
     try{
