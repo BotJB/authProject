d9d02d38c9811c7003db5ebc54aa1eda77778c37
jasjot Singh <jasjotb589@gmail.com>
2021-06-10 15:47:52 +0530

    email duplicates check ensured

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,7 +11,12 @@ router.post('/resgistration',async(req,res)=>{
        res.status(400).send(error.details[0].message)
        return;
    }
-   
+   //to see if the email adress already exits
+  const emailExist=await userModel.findOne({email:req.body.email})
+  if(emailExist){
+      res.send('sorry the email is already associated with another account')
+      return
+  }
 
 
     
